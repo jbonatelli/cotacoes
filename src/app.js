470de09fcde27a136441c802b77f329c2de88c83
jbonatelli <jbonatelli@Spectrum.local>
2019-06-19 13:40:55 -0300

    alterando porta e uri do app.js

--- src/app.js
+++ src/app.js
@@ -72,6 +72,8 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Running on port 3000')
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
 })
